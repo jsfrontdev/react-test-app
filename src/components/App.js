17c5bf7fd35709733eee8aebbe4c9f85cbb5b22c
jsfrontdev <EbltTestDev@mail.ru>
2019-03-29 18:01:38 +0300

    test-react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Canvas from './canvas';
+import Canvas from './Canvas';
 import PageLoader from './PageLoader';
 
 class App extends Component{
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import ReactDOM from 'react-dom';
 //import Canvas from './components/canvas';
 import './static/stylus/common.styl';
 import './static/css/common.css';
-import './static/stylus/index.styl';
-import './static/css/index.css';
+import './static/stylus/Canvas.styl';
+import './static/css/Canvas.css';
 //import PageLoader from './components/PageLoader';
 import App from './components/App';
 
